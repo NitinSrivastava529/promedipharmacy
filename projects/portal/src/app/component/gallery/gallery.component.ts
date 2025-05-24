f5fb483b19bc56387e1384153d7bfa961dc4836d
Nitin Srivastava <63593087+NitinSrivastava529@users.noreply.github.com>
2025-05-24 20:07:11 +0530

    OK

--- projects/portal/src/app/component/gallery/gallery.component.ts
+++ projects/portal/src/app/component/gallery/gallery.component.ts
@@ -13,21 +13,28 @@ import { FormsModule } from '@angular/forms';
 })
 export class GalleryComponent implements OnInit {
   productList: any;
+   productQuickView: any;
   _http = inject(HttpClient)
   _global = inject(GlobalService)
   constructor() { }
   ngOnInit(): void {
     this._global.loadScript();
-    this.productList=['cialis 20 mg.jpg','cialis 40 mg.jpg','oxycodone.jpg','percocet.png','soma 350 mg.jpg','soma 500 mg.jpg','trakem tramadol.jpg','tramadol 100 mg.jpg','tramadol.jpg','ultram tramadol 100mg.jpg','viagra 100.jpg','viagra 200.jpg']
-    //this.GetProduct()
+    //this.productList=['cialis 20 mg.jpg','cialis 40 mg.jpg','oxycodone.jpg','percocet.png','soma 350 mg.jpg','soma 500 mg.jpg','trakem tramadol.jpg','tramadol 100 mg.jpg','tramadol.jpg','ultram tramadol 100mg.jpg','viagra 100.jpg','viagra 200.jpg']
+    this.GetProduct()
   }
-     getImgUrl(file: string) {
-      return CONSTANT.ROOT_URL+'assets/product/' + file;
+  QuickView(productId: string) {
+    var val = this.productList.filter((i: any) => i.productId == productId)
+    this.productQuickView = val[0];
+  }
+     getImgUrl(file:string) {
+     //console.log(file)
+    return CONSTANT.API_URL + 'Resource/Product/' + file;
      // return this.sanitizer.bypassSecurityTrustResourceUrl(CONSTANT.API_URL+'/Resource/Plan/' + file);
     }
   GetProduct() {
     this._http.get(CONSTANT.API_URL + 'api/Product/GetProduct').subscribe((res: any) => {
       this.productList = res;
+    //  this.productList = this.productList.map((i:any)=>i.photo[0]);      
     })
   }
 }
